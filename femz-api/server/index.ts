93f1623f6c4f205d17350238b23e0f325899c08b
Nick Femia <femianicholas@gmail.com>
2022-11-25 17:11:07 -0500

    add operation protection

--- femz-api/server/index.ts
+++ femz-api/server/index.ts
@@ -1,5 +1,4 @@
 import { ApolloServer, BaseContext } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import { Prisma, PrismaClient, User } from "@prisma/client";
 import { loadSchemaSync } from "@graphql-tools/load";
 import { loadFilesSync } from "@graphql-tools/load-files";
@@ -7,6 +6,13 @@ import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { mergeResolvers } from "@graphql-tools/merge";
 import path from "path";
 import type { Resolvers } from "../gql/resolvers/resolvers-types";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import http from "http";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { GraphQLError } from "graphql";
 
 export interface GQLContext extends BaseContext {
   user: User | null;
@@ -17,8 +23,12 @@ export interface GQLContext extends BaseContext {
   >;
 }
 
+const unauthorizedOperations = ["LoginUser", "SignupUser"];
+
 (async function main() {
+  const app = express();
   const prisma = new PrismaClient();
+  const httpServer = http.createServer(app);
 
   const typeDefs = await loadSchemaSync(
     path.join(__dirname, "../gql/typeDefinitions/schema.graphql"),
@@ -35,22 +45,45 @@ export interface GQLContext extends BaseContext {
   const server = new ApolloServer<GQLContext>({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
+  await server.start();
 
-  const { url } = await startStandaloneServer<GQLContext>(server, {
-    listen: { port: 4000 },
-    context: async ({ req }) => {
-      const token = req.headers.authorization;
+  app.use(
+    "/",
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware<GQLContext>(server, {
+      context: async ({ req }) => {
+        const token = req.headers.authorization;
 
-      const user = await prisma.user.findUnique({
-        where: {
-          token,
-        },
-      });
+        const user = token
+          ? await prisma.user.findUnique({
+              where: {
+                token,
+              },
+            })
+          : null;
 
-      return { user, prisma };
-    },
-  });
+        if (!unauthorizedOperations.includes(req.body.operationName) && !user) {
+          throw new GraphQLError("User must be logged in.", {
+            extensions: {
+              code: "UNAUTHENTICATED",
+              http: {
+                stauts: 401,
+              },
+            },
+          });
+        }
 
-  console.log(`🚀  Server ready at: ${url}`);
+        return {
+          user,
+          prisma,
+        };
+      },
+    })
+  );
+
+  await httpServer.listen({ port: 4000 });
+  console.log("Apollo server started");
 })();
