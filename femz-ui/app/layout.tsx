0aeca76f46caebc15abc167be4794f3d9c80f0cf
Nick Femia <femianicholas@gmail.com>
2022-11-24 21:26:32 -0500

    apollo client

--- femz-ui/app/layout.tsx
+++ femz-ui/app/layout.tsx
@@ -1,4 +1,11 @@
+"use-client";
 import "../styles/dist.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className=" bg-slate-200 min-w-screen min-h-screen place-content-center">
-        {children}
+        <ApolloProvider client={client}>{children}</ApolloProvider>
       </body>
     </html>
   );
